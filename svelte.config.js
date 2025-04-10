95c677103bcf891b78bd42477971c3a7b2759fc2
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-10 16:24:56 +0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
