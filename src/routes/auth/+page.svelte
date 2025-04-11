0fc9e578ee99b5ce181c210aaf2bcdf8d2814c28
NetSMG <150570330+netsmg@users.noreply.github.com>
2025-04-11 21:18:18 +0600

    Create +page.svelte

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import PageTitle from '$components/common/PageTitle.svelte';
+	import ContactForm from '$components/auth/AuthForm.svelte';
+	import SEO from '$components/seo/SEO.svelte';
+
+	const breadcrumbs = [
+		{
+			name: 'Home',
+			slug: ''
+		},
+		{
+			name: 'Auth',
+			slug: 'auth'
+		}
+	];
+
+	const seoProps = {
+		breadcrumbs,
+		metadescription: 'Login or register.',
+		slug: 'auth',
+		title: 'Auth'
+	};
+</script>
+
+<SEO {...seoProps} />
+
+<PageTitle pageTitle="Auth" />
+
+<ContactForm />
